6fe0c062524903583fc5a8c93618658f5d3b54ea
Tiresias <fguzmanrs@gmail.com>
2020-05-07 19:29:36 -0700

    Fix functions

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
 $(document).ready(function () {
 
-    // Function
-
     function show(data) {
-    // temp (F), humidity (%), wind (mph)
+        // temp (F), humidity (%), wind (mph)
         return "<h2>" + data.name + moment().format(' (MM/DD/YYYY)') + "</h2>" +
             `
         <p><strong>Temperature</strong>: ${data.main.temp} °F</p>
@@ -13,7 +10,6 @@ $(document).ready(function () {
         `
     }
 
-    // UV displayed as bold, utilize lat and log var (class act)
     function showUV(data) {
         var lat = data.coord.lat;
         var lon = data.coord.lon;
@@ -23,74 +19,83 @@ $(document).ready(function () {
         `
     }
 
-    // city list, local storage
     function displayCities(cityList) {
-        $('.cityResults').empty();
+        $('.cityList').empty();
         var list = localStorage.getItem("cityList");
         cityList = (JSON.parse(list));
-        // string returned, js function to parse cityList
         if (list) {
             for (var i = 0; i < cityList.length; i++) {
                 var container = $("<div class=card></div>").text(cityList[i]);
-                $('.cityResults').prepend(container);
+                $('.cityList').prepend(container);
             }
         }
     }
 
     function showForecast(data) {
-        var forecast
-        // Obj to display date, icon, temp, humidity ; html display
-
-        // Variables for each requested data category
-        // convert temp F (class act)
-        var temp 
-        var humidity
-        var date 
-        var icon
-        var iconUrl
-
-        let htmlDisplay = `
-            <div class="col-sm weatherCondition">
-            <div class="card">
-                <div class="card-body 5d">
-                    <p><strong>date</strong></p>
-                    <div><img src=icon-url/></div>
-                    <p>Temp: °F</p>
-                    <p>Humidity:%</p>
-                </div>
-            </div> 
-        </div>`;
-        //push info into htmlDisplay
+        var forecast = data.list;
+        var currentForecast = [];
+        for (var i = 0; i < forecast.lenght; i++) {
+            var currentObject = forecast[i];
+            // 1:0, 2:-1, 3:-2
+            var dt_time = currentObject.dt_txt.split(' ')[1] // 1 is index
+            if (dt_time === "12:00:00") {
+                var main = currentObject.main;
+                var temp = main.temp; // *C -> *F
+                var humidity = main.humidity;
+                var date = moment(currentObject.dt_txt).format('1'); // momentjs conversion
+                var icon = currentObject.weather[0].icon;
+                var iconUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
+            
+                let htmlDisplay = `
+                    <div class="col-sm currentWeather">
+                        <div class="card">
+                            <div class="card-body 5day">
+                                <p><strong>${date}</strong></p>
+                                <div><img src=${iconUrl}/></div>
+                                <p>Temp: ${temp} °F</p>
+                                <p>Humidity: ${humidity}%</p>
+                            </div>
+                        </div> 
+                    </div>`;
+                currentForecast.push(htmlDisplay);
+            }
+        }
+        $("#5day-forecast").html(currentForecast.join(''));
     }
-
-}
 
 // Method
-
-var stored = localStorage.getItem("cityList")
-if (stored) {
-    cityList = JSON.parse(stored)
-} else {
-    cityList = []
-}
-
-// var cityList 
-$('#submitCity').click(function (event) {
-    // prevent default action!
-    event.preventDefault();
-    var city = $('#city').val();
-
-    // city gets pushed into cityList array
-    // cityList set in localStorage
-    // displayCities(cityList) if (AJAX)...else (error, cityName input req)
-        // ajax, reference class act
-            // $.ajax({
-            //     url: 'openWeather API' + city + "&units=imperial",
-            //     type: "GET",
-            //     success: function (data) {
-            //         var display = show(data);
-            //         $("#show").html(display);
-            //     }
-            // });            
-        // 
+    var stored = localStorage.getItem("cityList")
+    if (stored) {
+        cityList = JSON.parse(stored)
+    } else {
+        cityList = []
     }
+
+    $('#submitCity').click(function (event) {
+        event.preventDefault();
+        var city = $('#city').val();
+        cityList.push(city);
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+        displayCities(cityList);
+        if (city != '') {
+            $.ajax({
+                // url: api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+                type: "GET",
+                success: function (data) {
+                    var display = show(data);
+                    $("#show").html(display);
+                }
+            });
+
+            // $.ajax({}) showForecast
+
+            //$.ajax({}) showUV
+
+        } else {
+            $('#error').html('Please insert a city:');
+        }
+    });
+
+    displayCities(cityList);
+
+});
